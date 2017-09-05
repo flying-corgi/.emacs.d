b71ab8f2b99eaa3f61bf67611226a49b91018409
yz <flying.corgi.net@gmail.com>
2017-09-06 05:46:26 +0900

    fix complement

---
+++ inits/00-util.el
@@ -0,0 +1,8 @@
+;; ウィンドウ分割を便利に
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
+
+(global-set-key (kbd "C-t") 'other-window-or-split)
--- inits/30-complement.el
+++ inits/30-complement.el
@@ -1,3 +1,5 @@
+(global-company-mode +1)
+
 ;; C-n, C-pで補完候補を次/前の候補を選択
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
